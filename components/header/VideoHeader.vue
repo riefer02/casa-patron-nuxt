dd45becf64a28a5cc57cc13b42fe6905eecf8b54
riefer02 <andrew.riefenstahl@gmail.com>
2021-04-17 14:05:00 -0500

    loading spinner

--- components/header/VideoHeader.vue
+++ components/header/VideoHeader.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <div v-if="loading" class="bg-yellow-500 bg-opacity-25 h-screen">
-      <TextHeader />
-    </div>
     <video-background
-      v-else
       :src="require('@/assets/video/Casa-Patron-Splash-1.mp4')"
       style="height: 600px"
     >
@@ -21,14 +17,7 @@ export default {
     TextHeader,
   },
   data() {
-    return {
-      loading: true,
-    }
-  },
-  mounted() {
-    window.addEventListener('load', () => {
-      this.loading = !this.loading
-    })
+    return {}
   },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,33 @@
 <template>
-  <div class="flex flex-wrap overflow-hidden bg-yellow-300 linear-gradient">
-    <div class="w-full overflow-hidden">
-      <SideDrawer />
-    </div>
-    <div class="w-full overflow-hidden shadow-md">
-      <Header />
-    </div>
-    <div class="container mx-auto md:px-4 md:py-4">
-      <div id="about" class="w-full overflow-hidden"><About /></div>
-      <div id="features" class="w-full overflow-hidden"><Features /></div>
-      <div id="events" class="w-full overflow-hidden"><EventsList /></div>
-      <div id="gallery" class="w-full overflow-hidden"><Gallery /></div>
-      <div id="testimonials" class="w-full overflow-hidden">
-        <Testimonial />
+  <div>
+    <Loader v-if="loading" />
+    <div
+      v-else
+      class="flex flex-wrap overflow-hidden bg-yellow-300 linear-gradient"
+    >
+      <div class="w-full overflow-hidden">
+        <SideDrawer />
       </div>
-      <div id="booking" class="w-full overflow-hidden"><Booking /></div>
+      <div class="w-full overflow-hidden shadow-md">
+        <Header />
+      </div>
+      <div class="container mx-auto md:px-4 md:py-4">
+        <div id="about" class="w-full overflow-hidden"><About /></div>
+        <div id="features" class="w-full overflow-hidden"><Features /></div>
+        <div id="events" class="w-full overflow-hidden"><EventsList /></div>
+        <div id="gallery" class="w-full overflow-hidden"><Gallery /></div>
+        <div id="testimonials" class="w-full overflow-hidden">
+          <Testimonial />
+        </div>
+        <div id="booking" class="w-full overflow-hidden"><Booking /></div>
+      </div>
+      <div class="w-full overflow-hidden"><Footer /></div>
     </div>
-    <div class="w-full overflow-hidden"><Footer /></div>
   </div>
 </template>
 
 <script>
+import Loader from '@/components/utility/Loader.vue'
 import Header from '@/components/header/Header.vue'
 import About from '@/components/about/About.vue'
 import Features from '@/components/features/Features.vue'
@@ -34,6 +41,7 @@ import Gallery from '@/components/gallery/Gallery.vue'
 export default {
   name: 'Home',
   components: {
+    Loader,
     Gallery,
     Header,
     About,
@@ -43,6 +51,16 @@ export default {
     Booking,
     Footer,
     SideDrawer,
+  },
+  data() {
+    return {
+      loading: true,
+    }
+  },
+  mounted() {
+    window.addEventListener('load', () => {
+      this.loading = false
+    })
   },
 }
 </script>
